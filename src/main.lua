91975e12684d512c65f1f2d3c9c2fc65c75407dd
rm-a0 <michalrepcik9@gmail.com>
2025-05-02 15:33:13 +0200

    feat: add visualization

--- src/main.lua
+++ src/main.lua
@@ -6,13 +6,28 @@ local draw = require("render.draw")
 local _city
 local _generator
 local tileSize = 10
+local step = 1
+local time = 0
+local delay = 0.5
 
 function love.load()
     _city = city.new(500, 500)
     _generator = generator.new()
-    _generator:generateLakes(_city)
-    _generator:generateRivers(_city)
-    _generator:generateCityZones(_city)
+end
+
+function love.update(dt)
+    time = time + dt
+    if time > delay then
+        if step == 1 then
+            _generator:generateLakes(_city)
+        elseif step == 2 then
+            _generator:generateRivers(_city)
+        elseif step == 3 then
+            _generator:generateCityZones(_city)
+        end
+        step = step + 1
+        time = 0
+    end
 end
 
 function love.draw()
